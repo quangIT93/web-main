617ece3e7c85dad214153d2c4936759f0e58e9ad
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2023-04-25 11:31:54 +0700

    fix img

--- src/pages/PostDetail/route.tsx
+++ src/pages/PostDetail/route.tsx
@@ -3,7 +3,7 @@ import Detail from '.'
 import RouteProps from '../routes'
 const route: RouteProps = {
     path: '/post-detail',
-    component: <Detail />,
+    component: <></>,
 }
 
-//export default route
+export default route
