c876065df6eaa1ca0e00cbf4a9ba8bbc2a14ee5c
nhattan141 <trngtn44@gmail.com>
2023-07-13 13:10:20 +0700

    scroll top button

--- src/components/RollTop/index.tsx
+++ src/components/RollTop/index.tsx
@@ -2,9 +2,52 @@ import React, { FC } from 'react';
 
 import './styles.scss';
 
+import { ArrowUpOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+
 const RollTop: React.FC = () => {
+    const [height, setHeight] = React.useState(0);
+
+    const handleRollTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    const listenToScroll = () => {
+        const winScroll = document.body.scrollTop ||
+            document.documentElement.scrollTop;
+        setHeight(winScroll);
+    };
+
+    React.useEffect(() => {
+        window.addEventListener("scroll", listenToScroll);
+        return () =>
+            window.removeEventListener("scroll", listenToScroll);
+    }, [])
+
+    console.log("height", height);
+
+
     return (
-        <div className=''></div>
+        <div className="roll-top-container"
+
+        >
+            <Button
+                type="primary"
+                className="roll-top-btn"
+                shape="circle"
+                icon={<ArrowUpOutlined />}
+                onClick={handleRollTop}
+                style={
+                    height > 200
+                        ? {
+                            bottom: '60px',
+                        }
+                        : {
+                            bottom: '-60px',
+                        }
+                }
+            />
+        </div>
     )
 }
 
