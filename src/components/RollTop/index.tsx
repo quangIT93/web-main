49472556671873c094ac9b6184f8228fe8053ac6
nhattan141 <trngtn44@gmail.com>
2023-07-13 10:20:58 +0700

    fix ui, title post detail

---
+++ src/components/RollTop/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+import './styles.scss';
+
+const RollTop: React.FC = () => {
+    return (
+        <div className=''></div>
+    )
+}
+
+export default RollTop;
