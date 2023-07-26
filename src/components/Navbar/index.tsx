84e00a5b7c21689b4a276d5a2422e55e00749071
quangdev <quangbk5493@gmail.com>
2023-07-26 18:12:34 +0700

    deploy eorr

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -97,15 +97,15 @@ const Navbar: React.FC = () => {
     setOpenNotificate,
     openNotificate,
   }: // setRefNav,
-    {
-      openCollapseFilter: boolean;
-      setOpenCollapseFilter: React.Dispatch<React.SetStateAction<boolean>>;
-      // heightNavbar: number
-      // setHeightNavbar: React.Dispatch<React.SetStateAction<number>>
-      SetRefNav: React.Dispatch<React.SetStateAction<DivRef1>>;
-      setOpenNotificate: React.Dispatch<React.SetStateAction<boolean>>;
-      openNotificate: boolean;
-    } = useContext(HomeValueContext);
+  {
+    openCollapseFilter: boolean;
+    setOpenCollapseFilter: React.Dispatch<React.SetStateAction<boolean>>;
+    // heightNavbar: number
+    // setHeightNavbar: React.Dispatch<React.SetStateAction<number>>
+    SetRefNav: React.Dispatch<React.SetStateAction<DivRef1>>;
+    setOpenNotificate: React.Dispatch<React.SetStateAction<boolean>>;
+    openNotificate: boolean;
+  } = useContext(HomeValueContext);
 
   const {
     receivedMessages,
@@ -463,21 +463,23 @@ const Navbar: React.FC = () => {
     setTimeout(() => {
       window.open(
         `/search-results?${encode !== 'undefined' ? `q=${encode}` : ``}` +
-        `${salary_type ? `&sal-type=${salary_type}` : ''}` +
-        `${job_type ? `&job-type=${job_type}` : ''}` +
-        `${params.toString() !== '' ? `&${params.toString()}` : ''}` +
-        `${list_cate.length > 0
-          ? `&${paramsCate.toString()}`
-          : `&${paramsCate.toString()}`
-        }` +
-        `${salary_min ? `&salary_min=${salary_min}` : ''}` +
-        `${salary_max ? `&salary_max=${salary_max}` : ''}` +
-        `${is_working_weekend
-          ? `&is_working_weekend=${is_working_weekend}`
-          : ''
-        }` +
-        `${is_remotely ? `&is_remotely=${is_remotely}` : ''}` +
-        `${money_type ? `&money_type=${money_type}` : ''}`,
+          `${salary_type ? `&sal-type=${salary_type}` : ''}` +
+          `${job_type ? `&job-type=${job_type}` : ''}` +
+          `${params.toString() !== '' ? `&${params.toString()}` : ''}` +
+          `${
+            list_cate.length > 0
+              ? `&${paramsCate.toString()}`
+              : `&${paramsCate.toString()}`
+          }` +
+          `${salary_min ? `&salary_min=${salary_min}` : ''}` +
+          `${salary_max ? `&salary_max=${salary_max}` : ''}` +
+          `${
+            is_working_weekend
+              ? `&is_working_weekend=${is_working_weekend}`
+              : ''
+          }` +
+          `${is_remotely ? `&is_remotely=${is_remotely}` : ''}` +
+          `${money_type ? `&money_type=${money_type}` : ''}`,
         '_self',
       );
     }, 100);
@@ -659,9 +661,9 @@ const Navbar: React.FC = () => {
               <Link to="/history" target="_parent">
                 <div
                   className="sub-login_item"
-                // onClick={() => {
-                //   window.open('/history', "_top")
-                // }}
+                  // onClick={() => {
+                  //   window.open('/history', "_top")
+                  // }}
                 >
                   <ClockCircleOutlined />
                   <span>Lịch sử</span>
@@ -749,9 +751,9 @@ const Navbar: React.FC = () => {
               <Link to="/history">
                 <div
                   className="sub-login_item"
-                // onClick={() => {
-                //   window.open('/history', "_top")
-                // }}
+                  // onClick={() => {
+                  //   window.open('/history', "_top")
+                  // }}
                 >
                   <ClockCircleOutlined />
                   <span>Lịch sử</span>
@@ -774,8 +776,9 @@ const Navbar: React.FC = () => {
 
   return (
     <div
-      className={`modal-navbar ${openCollapseFilter ? 'show-modal_navbar' : ''
-        }`}
+      className={`modal-navbar ${
+        openCollapseFilter ? 'show-modal_navbar' : ''
+      }`}
     >
       <Container className="nav" ref={ref}>
         <ModalLogin
