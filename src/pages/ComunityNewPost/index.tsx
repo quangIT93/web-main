7e5028919a1cf61da27a2180f2884f41d52cb069
nhattan141 <trngtn44@gmail.com>
2023-08-31 16:38:17 +0700

    fix bug

--- src/pages/ComunityNewPost/index.tsx
+++ src/pages/ComunityNewPost/index.tsx
@@ -200,21 +200,30 @@ const ComunityNewPost = () => {
       <div className="comunity-content">
         <div className="comunityPostNew">
           <div className="title-comunity">
-            <h3>
-              {
-                loading
-                  ? 'Loading...'
-                  : languageRedux === 1
-                    ? `Hôm nay, HiJob có ${total} bài viết mới`
-                    : `Today, HiJob has ${total} new posts`
+            <div className='title-comunity-new-content'>
+              <h3>
+                {
+                  languageRedux === 1
+                    ? `Câu chuyện công việc`
+                    : `Working story`
+                }
+              </h3>
+              <p>
+                {
+                  loading
+                    ? 'Loading...'
+                    : languageRedux === 1
+                      ? `${new Intl.NumberFormat('en-US').format(total)} bài viết mới`
+                      : `${new Intl.NumberFormat('en-US').format(total)} new posts`
 
-                // language?.community_page?.today_hijob_has +
-                //   ' ' +
-                //   total +
-                //   ' ' +
-                //   language?.community_page?.new_posts
-              }
-            </h3>
+                  // language?.community_page?.today_hijob_has +
+                  //   ' ' +
+                  //   total +
+                  //   ' ' +
+                  //   language?.community_page?.new_posts
+                }
+              </p>
+            </div>
             <div className="title-comunity_icon">
               <div
                 className="dropdown dropdown-4"
--- src/pages/DetailComunity/index.tsx
+++ src/pages/DetailComunity/index.tsx
@@ -99,7 +99,7 @@ const Comunity = () => {
       if (result.status === 200) {
         setDeleteCmt(!deleteCmt);
       }
-    } catch (error) {}
+    } catch (error) { }
   };
 
   const handleGetDetailCommunityById = async () => {
@@ -144,9 +144,8 @@ const Comunity = () => {
     console.log('image', image);
     return {
       src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-      srcSet: `${image}?w=${size * cols}&h=${
-        size * rows
-      }&fit=crop&auto=format&dpr=2 2x`,
+      srcSet: `${image}?w=${size * cols}&h=${size * rows
+        }&fit=crop&auto=format&dpr=2 2x`,
     };
   };
 
@@ -298,8 +297,8 @@ const Comunity = () => {
     fromHistory === '31'
       ? window.open('/history?community_post=31', '_parent')
       : fromHistory === '30'
-      ? window.open('/history?community_post=30', '_parent')
-      : window.open(
+        ? window.open('/history?community_post=30', '_parent')
+        : window.open(
           detail?.type === 1 ? '/new-comunity' : '/news-comunity',
           '_parent',
         );
@@ -320,12 +319,12 @@ const Comunity = () => {
               {fromHistory === '31' || fromHistory === '30'
                 ? language?.history
                 : detail?.type === 1
-                ? languageRedux === 1
-                  ? 'Câu chuyện việc làm'
-                  : 'Working story'
-                : languageRedux === 1
-                ? 'Tin tức HiJob'
-                : 'HiJob news'}
+                  ? languageRedux === 1
+                    ? 'Câu chuyện việc làm'
+                    : 'Working story'
+                  : languageRedux === 1
+                    ? 'Tin tức HiJob'
+                    : 'HiJob news'}
             </h3>
           </div>
           <div className="title-comunity">
@@ -360,7 +359,7 @@ const Comunity = () => {
                 }
                 icon={<UserOutlined />}
                 style={{
-                  filter: detail?.type === 1 ? 'blur(1px)' : 'none',
+                  filter: detail?.type === 1 ? 'blur(3px)' : 'none',
                 }}
               />
               <div className="info-actor_comunityDetail">
@@ -380,7 +379,7 @@ const Comunity = () => {
               <TextArea
                 value={detail?.content}
                 autoSize
-                // showCount
+              // showCount
               />
             </div>
           </div>
@@ -552,7 +551,7 @@ const Comunity = () => {
                       : 'Enter your comment ...'
                   }
                   autoSize
-                  // showCount
+                // showCount
                 />
                 <div className="comment-interaction">
                   <div
@@ -604,7 +603,7 @@ const Comunity = () => {
                                 display:
                                   detail?.profileData?.id ===
                                     localStorage.getItem('accountId') ||
-                                  cmtData?.profile?.id ===
+                                    cmtData?.profile?.id ===
                                     localStorage.getItem('accountId')
                                     ? 'block'
                                     : 'none',
@@ -620,7 +619,7 @@ const Comunity = () => {
                             <TextArea
                               value={cmtData?.content}
                               autoSize
-                              // showCount
+                            // showCount
                             />
                             {/* <p>{cmtData?.content}</p> */}
                           </div>
