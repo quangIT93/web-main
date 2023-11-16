ba947ca42cbc3d9d06b7c06ee9055893b8ee279d
quangbk5493 <quangbk5493@gmail.com>
2023-11-16 16:41:31 +0700

    vali company

---
+++ src/pages/Post/CheckPost/PostProtect.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { RootState } from 'store';
+
+interface Props {
+    children?: React.ReactNode
+  }
+const PostProtect = ({children} : Props) => {
+    const profileV3 = useSelector(
+        (state: RootState) => state.dataProfileInformationV3.data,
+      );
+
+                                                        if(profileV3?.companyInfo?.status === 1){
+        const url = `/`
+        return <Navigate to={url} />;
+      }
+    
+    return children ? <>{children}
+    
+    </> : null;
+}
+
+export default PostProtect
